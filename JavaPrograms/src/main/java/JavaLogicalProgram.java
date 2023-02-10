fa0d0cbb9d4554881e944ead6a696250d90b726f
ganesh19gani <97647488+ganesh19gani@users.noreply.github.com>
2023-02-10 08:08:46 +0530

    String_Logical_Program changes
    String_Logical_Program changes

---
+++ JavaPrograms/src/main/java/JavaLogicalProgram.java
@@ -0,0 +1,12 @@
+
+
+// To Revese Word by Word
+public class JavaLogicalProgram {
+
+    public static void main(String args[]) {
+        String str = "I am Ganesh";
+        String arr[] = str.split(" ");
+        System.out.println(arr[2] + " " + arr[1] + " " + arr[0]);
+
+    }
+}
